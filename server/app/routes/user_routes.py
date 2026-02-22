d4ec4054346743cf5aaa9f1b5638b4b2227cb541
Moheeb <moheebk522@gmail.com>
2026-02-22 15:33:31 +0530

    Refactor check_user endpoint to manage user sessions with cookies

--- server/app/routes/user_routes.py
+++ server/app/routes/user_routes.py
@@ -1,8 +1,29 @@
-from fastapi import APIRouter
-from app.controllers.user_controller import check_or_create_user
+from fastapi import APIRouter, Request, Response
+from app.controllers.user_controller import get_or_create_user
+import time
+import random
 
 router = APIRouter()
 
 @router.get("/check-user")
-def check_user(userId: str):
-    return check_or_create_user(userId)
+def check_user(request: Request, response: Response):
+    # 👇 cookie read
+    user_id = request.cookies.get("portfolio-chat")
+
+    # ❌ if cookie not exists → new user
+    if not user_id:
+        user_id = str(int(time.time() * random.random()))
+        user = get_or_create_user(user_id)
+
+        # 👇 cookie set (no expiry = session cookie)
+        response.set_cookie(
+            key="portfolio-chat",
+            value=user_id,
+            httponly=False,
+            samesite="lax"
+        )
+        return user
+
+    # ✅ if cookie exist  → existing user
+    user = get_or_create_user(user_id)
+    return user
