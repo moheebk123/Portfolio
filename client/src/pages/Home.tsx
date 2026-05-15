35eef22bd3a7e412002f76e694f3ab57d78e70eb
Moheeb Khan <113541342+moheebk123@users.noreply.github.com>
2026-05-16 01:20:02 +0530

    Update resume link in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -74,7 +74,7 @@ const Home = () => {
           className="flex space-x-4"
         >
           <a
-            href="https://drive.google.com/file/d/1mM3Cv-Slp7PgOkgsy0tPOs_pwc5fRsH_/view?usp=sharing"
+            href="https://drive.google.com/file/d/1WTf0GXOc5Ed1tu_9BmCxiAsBT52wHG5y/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-zinc-100 text-black font-semibold py-2 px-6 rounded-md shadow-lg hover:bg-transparent hover:border hover:border-white hover:text-white hover:scale-110 ease-in-out duration-300 transition"
