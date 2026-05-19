c60513e5da6a70ef3435c11660e339fb4515b8c4
Moheeb <moheebk522@gmail.com>
2026-05-19 22:14:16 +0530

    add new project

--- client/src/data/projects.ts
+++ client/src/data/projects.ts
@@ -1,3 +1,4 @@
+import tripFoldZeno from "../assets/img/projects/trip-fold-zeno.png"
 import urlShortener from "../assets/img/projects/url-shortener.png"
 import taskBoard from "../assets/img/projects/task-board.png"
 import css from "../assets/img/skills/css.png";
@@ -15,6 +16,16 @@ interface Project {
 
 export const projects: Project[] = [
   {
+    heading: "Trip Fold Zeno",
+    description:
+      "A modern Trip Expense Calculator to manage shared trip expenses, meals, and participant-based balance calculations.",
+    liveLink: "https://trip-fold-zeno-official.vercel.app",
+    skills:
+      "React, React Router, Redux Toolkit, TypeScript, Tailwind CSS, Shadcn",
+    imgUrl: tripFoldZeno,
+    alt: "Trip Fold Zeno Logo",
+  },
+  {
     heading: "Url Shortener",
     description:
       "URL Shortener built with Node.js, Express, and MongoDB that allows users to generate short links, manage them, and handle secure redirection.",
@@ -29,8 +40,7 @@ export const projects: Project[] = [
     description:
       "Built a real-time collaborative task board using MERN and Socket.IO with live drag-and-drop, smart assignment, and conflict handling for seamless multi-user task management.",
     liveLink: "https://to-do-board-one.vercel.app",
-    skills:
-      "React, Redux Toolkit, Node.js, Express.js, Socket.IO, MongoDB",
+    skills: "React, Redux Toolkit, Node.js, Express.js, Socket.IO, MongoDB",
     imgUrl: taskBoard,
     alt: "Real-Time Collaborative Task Board",
   },
@@ -39,15 +49,14 @@ export const projects: Project[] = [
     description:
       "Developed a webpage showcasing 10 React projects with preview images, live links for direct access, and code repository links to highlight advanced React skills and provide detailed insights into each project.",
     liveLink: "https://moheeb-khan-react-projects.vercel.app",
-    skills:
-      "React.js, React Router, Redux Toolkit, Material UI, Appwrite",
+    skills: "React.js, React Router, Redux Toolkit, Material UI, Appwrite",
     imgUrl: react,
     alt: "React Logo",
   },
   {
     heading: "JavaScript Projects",
     description:
-    "Developed a webpage featuring 51 JavaScript projects with preview images, live links for direct access, and code repository links to showcase technical skills from basic to advanced levels.",
+      "Developed a webpage featuring 51 JavaScript projects with preview images, live links for direct access, and code repository links to showcase technical skills from basic to advanced levels.",
     liveLink: "https://moheeb-khan-javascript-projects.vercel.app",
     skills: "HTML, CSS, JavaScript",
     imgUrl: javascript,
